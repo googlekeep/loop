ff40d4bc16d54486df14c55510dbe3f47f18729b
googlekeep <info.workingforyou@gmail.com>
2022-09-13 01:39:24 +0530

    avareage

---
+++ apple-orange.js
@@ -0,0 +1,9 @@
+var mobeyGiven = 1000;
+var apple = 400;
+var orangePrice = 300;
+
+
+var expense = applePrice + orangePrice;
+console.log(totalPrice);
+var givenBack = mobeyGiven-totalPrice;
+console.log(givenBack);
